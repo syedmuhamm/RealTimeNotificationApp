d66ce59d81efb4196074367b292492cdd8275fcc
syedmuhammadzulqarnain2@gmail.com <syedmuhammzulqarnain2@gmail.com>
2021-11-03 16:55:14 +0100

    layout updated, with functionality added to card, with images and logos.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./app.css";
 import Card from "./components/card/Card";
 import Navbar from "./components/navbar/Navbar";
+import { posts } from "./data";
 
 const App = () => {
   const [username, setUsername] = useState("");
@@ -12,7 +13,9 @@ const App = () => {
       {user ? (
         <>
           <Navbar />
-          <Card />
+          {posts.map((post) => (
+            <Card key={post.id} post={post} />
+          ))}
           <span className="username">{user}</span>
         </>
       ) : (
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,26 @@
 import "./card.css";
+import Heart from "./../../img/heart.svg";
+import HeartFilled from "./../../img/heartFilled.svg";
+import Comment from "./../../img/comment.svg";
+import Share from "./../../img/share.svg";
+import Info from "./../../img/info.svg";
 
-const Card = () => {
-  return <div className="card">card</div>;
+const Card = ({ post }) => {
+  return (
+    <div className="card">
+      <div className="info">
+        <img src={post.userImg} alt="" className="userImg" />
+        <span>{post.fullname}</span>
+      </div>
+      <img src={post.postImg} alt="" className="postImg" />
+      <div className="interactions">
+        <img src={Heart} alt="" className="cardIcon" />
+        <img src={Comment} alt="" className="cardIcon" />
+        <img src={Share} alt="" className="cardIcon" />
+        <img src={Info} alt="" className="cardIcon" />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
